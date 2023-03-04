ffa65f101f09b5ca1c614f9f75da9e9893e6c954
alikazan <kazan.ali.97@gmail.com>
2023-03-04 16:24:29 +0100

    server

--- server.py
+++ server.py
@@ -9,7 +9,9 @@ from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
                                unset_jwt_cookies, jwt_required, JWTManager
 
 from flask import send_file
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -18,6 +20,36 @@ app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
 app.config["JWT_SECRET_KEY"] = "99999999999999999999"
 jwt = JWTManager(app)
 
+#DB
+db = SQLAlchemy()
+DB_NAME = "database.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+login_manager = LoginManager()
+
+
+class User(db.Model,UserMixin):
+    _id = db.Column(db.Integer, primary_key=True)
+    _email = db.Column(db.String(150), unique=True)
+    _password = db.Column(db.String(150), default='1234223')
+    _date=db.Column(db.DateTime(timezone=True), default=func.now())
+    def get_id(self):
+        return self._id
+    def get_email(self):
+        return self._email
+    def get_passsword(self):
+        return self._password
+#initialize          
+login_manager.init_app(app)
+def create_database(app):
+    if not path.exists(DB_NAME):
+       # db.create_all(app=app)  old
+       with app.app_context():
+        db.create_all()
+        print('Created Database!')
+ 
+create_database(app)
+
 
 #routes
 @app.route('/')
@@ -25,14 +57,69 @@ def attributes():
     print("Connected")
     return jsonify("connected!, try routes: /Dtree /knn /logREg")
 
-@app.route('/french')
+#return PDF
+@app.route('/report')
 def french():
     try:
         return send_file('Report.pdf', download_name='Report.pdf')
     except Exception as e:
         return str(e)
 
+#DB routes
+app.route('/login', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    query_email_exists = db.session.query(User).filter(User._email == email)
+    query_password = db.session.query(User).filter(User._email == email).first()
+    print(db.session.query(query_email_exists.exists()).scalar()) 
+    emailExists=db.session.query(query_email_exists.exists()).scalar()
+    if not emailExists:
+        return {"msg": "email does not exist"}, 401
+    if emailExists and query_password._password==password:
+        access_token = create_access_token(identity=email)
+        response = {"access_token":access_token}, 200
+        print(response)
+        return response
+    else:
+        return {"msg": "password is not correct"}, 402
 
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logged out successfully"})
+    unset_jwt_cookies(response)
+    print(response)
+    return response 
+
+@app.route("/users",methods=["GET"])
+def getAccounts():
+    #allUsers = User.query.all()
+    allUsers= db.session.query(User).all()
+    my_dict = {"_email":[],"_date":[],"_id":[]}
+    for u in allUsers:
+        my_dict["_email"].append(u.__dict__["_email"])
+        my_dict["_id"].append(u.__dict__["_id"])
+        my_dict["_date"].append(u.__dict__["_date"])
+    response=jsonify(my_dict)
+    return response
+
+@app.route('/signup',methods=['POST'])
+def signup():
+    #method edit to be post
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    new_userr = User(_email=email,_password=password)
+    query_email_exists = db.session.query(User).filter(User._email == email)
+    emailExists=db.session.query(query_email_exists.exists()).scalar()
+    if emailExists:
+        return {"msg": "You already Have an account"}, 401
+    else:
+        db.session.add(new_userr)
+        db.session.commit()
+        print('@@@@@@@@@@NEW user added to db @@@@@@@@@@@')
+        return {"msg": "Account created"}, 200    
+
+    
 
 
 if __name__=="__main__":
